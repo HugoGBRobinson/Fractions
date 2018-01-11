fbd6d8520f9438ebcf74a48a330d33f4b47c24f7
Hugo Robinson <hugogbrobinson@gmail.com>
2018-01-11 21:18:02 +0000

    Started work on Fractions Objects

---
+++ Fractions Test/Fraction/Class1.cs
@@ -0,0 +1,54 @@
+﻿using System;
+
+namespace Fractions
+{
+    public class Fraction
+    {
+        public Fraction(int int1, int int2)
+        {
+            int Numerator = int1;
+            int Denominator = int2;
+
+            
+        }
+
+        public int int1 { get; set; }
+        public int int2 { get; set; }
+        
+
+
+        public double plus(Fraction a, Fraction b)
+        {
+            double fraction1 = a.int1 / a.int2;
+            double fraction2 = b.int1 / b.int2;
+            var total = fraction1 + fraction2;
+            return total;
+        }
+        public double minus(Fraction b)
+        {
+            var total = b.int1 - b.int2;
+            return total;
+        }
+        public double multiplyBy(Fraction b)
+        {
+            var total = b.int1 * b.int2;
+            return total;
+        }
+        public double divideBy(Fraction b)
+        {
+            var total = b.int1 / b.int2;
+            return total;
+        }
+
+        public override string ToString()
+        {
+            return null;
+
+        }
+
+
+    }
+}
+
+
+
---
+++ Fractions Test/Fractions Test/UnitTest1.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using Fractions;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Fractions_Test
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void test_1()
+        {
+            var a = new Fraction(2, 3); // 2/3
+            var b = new Fraction(1, 4); // 1/4
+            Assert.AreEqual("11/12", a.plus(a,b).ToString());
+        }
+
+        [TestMethod]
+        public void test_2()
+        {
+            var a = new Fraction(2, 3);
+            var b = new Fraction(1, 4);
+            Assert.AreEqual("5/12", a.minus(b).ToString());
+        }
+
+        [TestMethod]
+        public void test_3()
+        {
+            var a = new Fraction(2, 3);
+            var b = new Fraction(1, 4);
+            Assert.AreEqual("2/12", a.multiplyBy(b).ToString());
+        }
+
+        [TestMethod]
+        public void test_4()
+        {
+            var a = new Fraction(2, 3);
+            var b = new Fraction(1, 4);
+            Assert.AreEqual("8/3", a.divideBy(b).ToString());
+        }
+    }
+}
