55a5e91de4e4f9851a60feb396e741199ffb6dd8
Hugo Robinson <hugogbrobinson@gmail.com>
2018-01-14 23:08:18 +0000

    Now returning correct values, but the tests still fail due to "null" bieng returned

--- Fractions Test/Fraction/Class1.cs
+++ Fractions Test/Fraction/Class1.cs
@@ -1,35 +1,40 @@
 ﻿using System;
 
+
 namespace Fractions
 {
     public class Fraction
     {
-        public Fraction(int int1, int int2)
+        public Fraction(int Num1, int Num2)
         {
-             Numerator = int1;
-             Denominator = int2;
-
+             Numerator = Num1;
+             Denominator = Num2;
+            var total = Numerator / Denominator;
             
         }
 
         public int Numerator { get; set; }
         public int Denominator  { get; set; }
-        
+
 
 
         public Fraction plus(Fraction b)
         {
-            double NewNumerator = Numerator + b.Numerator;
-            double NewDenominator = Denominator + b.Denominator;
-            Fraction total = new Fraction(Convert.ToInt32(NewNumerator), Convert.ToInt32(NewDenominator));
-            return total;
+
+            int NewDenominator = Denominator * b.Denominator;
+            int num1 = Numerator * b.Denominator;
+            int num2 = b.Numerator * Denominator;
+            int NewNumerator = num1 + num2;
+            var NewFunction = new Fraction(NewNumerator, NewDenominator);
+            return NewFunction;
         }
         public Fraction minus(Fraction b)
         {
-            double NewNumerator = Numerator - b.Numerator;
-            double NewDenominator = Denominator - b.Denominator;
-            Fraction total = new Fraction(Convert.ToInt32(NewNumerator), Convert.ToInt32(NewDenominator));
-            return total;
+            int NewDenominator = Denominator * b.Denominator;
+            int num1 = Numerator * b.Denominator;
+            int num2 = b.Numerator * Denominator;
+            int NewNumerator = num1 - num2;
+            return new Fraction(NewNumerator, NewDenominator);
         }
         public Fraction multiplyBy(Fraction b)
         {
@@ -41,7 +46,7 @@ namespace Fractions
         public Fraction divideBy(Fraction b)
         {
             double NewNumerator = Numerator / b.Numerator;
-            double NewDenominator = Denominator / b.Denominator;
+            double NewDenominator = Denominator * b.Denominator;
             Fraction total = new Fraction(Convert.ToInt32(NewNumerator), Convert.ToInt32(NewDenominator));
             return total;
         }
